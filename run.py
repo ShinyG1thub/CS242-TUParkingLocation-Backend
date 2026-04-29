7afe9024e99a7ef2909d08b12c8019c6a7ae4954
ShinyG1thub <atichol.rue@dome.tu.ac.th>
2026-04-29 17:09:56 +0700

    Update

--- run.py
+++ run.py
@@ -2,7 +2,8 @@
 
 from app import create_app
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     # Accessible from any device on the network (great for mobile testing)
     app.run(host="0.0.0.0", port=5000, debug=True)
